26c3784cc6f5e3b3a6ed68a3aaf424d5226ef53c
Vladimir Kireev <ubitu111@yandex.ru>
2020-01-19 13:25:07 +0700

    Внедрение заполнения анкеты добровольца ч.1

--- app/src/main/java/ru/kireev/mir/laregistrationclient/MainActivity.java
+++ app/src/main/java/ru/kireev/mir/laregistrationclient/MainActivity.java
@@ -1,11 +1,14 @@
 package ru.kireev.mir.laregistrationclient;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -148,5 +151,22 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, QRCodeActivity.class);
         intent.putExtra("message", markForQrCode);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_fill_out_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_action_profile_volunteer) {
+            Intent intent = new Intent(this, VolunteerProfileActivity.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/ru/kireev/mir/laregistrationclient/VolunteerProfileActivity.java
@@ -0,0 +1,14 @@
+package ru.kireev.mir.laregistrationclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VolunteerProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volunteer_profile);
+    }
+}
